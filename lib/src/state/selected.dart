0bc01cded5a8076e2d2597b40881ae94f7ff5cdc
sanit <sanit.jigsaw@gmail.com>
2022-08-21 22:01:26 +0700

    Update selected.dart

--- lib/src/state/selected.dart
+++ lib/src/state/selected.dart
@@ -20,6 +20,8 @@ abstract class S2Selected<T> extends ChangeNotifier {
 
   /// Returns the length of the [choice]
   int get length;
+  
+  bool _disposed = false;
 
   /// Returns `true` if there are no values in the selection
   bool get isEmpty;
@@ -87,6 +89,20 @@ abstract class S2Selected<T> extends ChangeNotifier {
 
   /// Return a [Text] widget from [toString]
   Widget toWidget() => Text(toString());
+  
+    @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
+  }
+
+  @override
+  void notifyListeners() {
+    if (!_disposed) {
+      super.notifyListeners();
+    }
+  }
+  
 }
 
 /// State of single selected choice
